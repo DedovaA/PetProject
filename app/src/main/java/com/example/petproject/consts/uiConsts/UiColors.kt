ca05722dabc5a1255416889f11fd07fdb2a8d068
asya <asyatim@mail.ru>
2022-06-09 12:48:04 +0600

    implemented login screen

---
+++ app/src/main/java/com/example/petproject/consts/uiConsts/UiColors.kt
@@ -0,0 +1,13 @@
+package com.example.petproject.consts.uiConsts
+
+import androidx.compose.ui.graphics.Color
+
+val mainBlue = Color(0xFF35AAFF)
+val main_deep_blue = Color(0xFF004475)
+val main_pink = Color(0xFFEF7096)
+val neutrals_light_gray = Color(0xFFD2D2D2)
+val neutrals_gray = Color(0xFFB3B3B3)
+val neutrals_deep_gray = Color(0xFF001829)
+val validation_light_gray = Color(0xFFD2D2D2)
+val validation_gray = Color(0xFF777777)
+val validation_deep_gray = Color(0xFF001829)
